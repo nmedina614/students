39cc38fd013a500881d16eacc35480d0617247d0
Nolan Medina <njmedina614@gmail.com>
2018-02-15 14:08:11 -0500

    startetd adding student list

--- index.php
+++ index.php
@@ -1,32 +1,21 @@
 <?php
-
-
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
-
-//Require the autoload file
+//Required files
 require_once('vendor/autoload.php');
-
-//Create an instance of the Base Class
+require('model/db-functions.php');
+//Create an instance of the Base class
 $f3 = Base::instance();
-$f3->set("DEBUG", 3);
-
-
-
-$f3 -> set('DEBUG', 3);
-//define a page1 route
-
-
-$f3->route('GET /', function() {
-
-    echo "hello";
-
-    /*$template = new Template();
-    echo $template->render('views/home.html');*/
-
+$f3->set('DEBUG', 3);
+//Connect to the database
+$dbh = connect();
+//Define a default route
+$f3->route('GET /', function($f3) {
+    $students = getStudents();
+    $f3->set('students', $students);
+    //load a template
+    $template = new Template();
+    echo $template->render('views/allstudents.html');
 });
-
-
-
-
+//Run fat free
 $f3->run();
---
+++ model/db-functions.php
@@ -0,0 +1,31 @@
+
+<?php
+require("/home/nmedinag/config/config.php");
+function connect()
+{
+    try {
+        //Instantiate a database object
+        $dbh = new PDO(DB_DSN, DB_USERNAME,
+            DB_PASSWORD);
+        echo "Connected to database!!!";
+        return $dbh;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return;
+    }
+}
+function getStudents()
+{
+    global $dbh;
+    //1. define the query
+    $sql = "SELECT * FROM student ORDER BY last, first";
+    //2. prepare the statement
+    $statement = $dbh->prepare($sql);
+    //3. bind parameters
+    //4. execute the statement
+    $statement->execute();
+    //5. return the result
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //print_r($result);
+    return $result;
+}
